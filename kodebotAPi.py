c94732b5b9f8ba8e4dfbfd31ed1101d0411137a9
SasKodland <139468320+SasKodland@users.noreply.github.com>
2023-09-14 20:42:56 +0700

    Create kodebotAPi.py
    tugas kode masukin url

---
+++ kodebotAPi.py
@@ -0,0 +1,60 @@
+import requests
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+import random, os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(8))
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        # Mari simpan file perpustakaan /library discord yang dikonversi dalam variable ini!
+        picture = discord.File (f)
+        f.close()
+    # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def cat(ctx):
+    img_name = random.choice(os.listdir('images2'))
+    with open(f'images2/{img_name}', 'rb') as f:
+        # Mari simpan file perpustakaan /library discord yang dikonversi dalam variable ini!
+        picture = discord.File (f)
+        f.close()
+    # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run("apa hayooo")
